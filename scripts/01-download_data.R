ecb28348a869b9d8bb559de88ce02126290a5e65
Yiliu Cao <l1023498789@gmail.com>
2023-10-01 23:14:45 -0400

    Initial commit

---
+++ scripts/01-download_data.R
@@ -0,0 +1,27 @@
+#### Preamble ####
+# Purpose: Downloads and saves the data from toronto open data portal
+# Author: Yiliu Cao
+# Date: 1 October 2023
+# Contact: yiliu.cao@mail.utoronto.ca
+# License: MIT
+# Pre-requisites: 
+# Any other information needed? [...UPDATE THIS...]
+
+
+#### Workspace setup ####
+library(opendatatoronto)
+library(tidyverse)
+
+#### Download data ####
+bicycle_thefts_package <- search_packages("Bicycle Thefts")
+bicycles_thefts_resources <- bicycle_thefts_package %>% 
+  list_package_resources() %>% 
+  filter(format == "CSV") %>% 
+  head(1)
+raw_thefts_data <- bicycles_thefts_resources %>% 
+  get_resource()
+
+#### Save data ####
+write_csv(raw_thefts_data, "inputs/data/raw_thefts_data.csv") 
+
+         
